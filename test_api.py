0ba00a315fd24aeff4e468c94a4215bd0cd54b9d
WinerGero <korykin.27@mail.ru>
2025-12-28 15:34:17 +0200

    maked empty test class

--- test_api.py
+++ test_api.py
@@ -20,13 +20,41 @@ def seller_id():
 # Create body for POST request for testing create item
 @pytest.fixture
 def item_body(seller_id):
+    """
+    Docstring for item_body
+    
+    :param seller_id: Description
+    :return: Description
+    :rtype: dict[str, Any]
+    """
     return {
-        "sellerID": "<integer>",
-        "name": "<string>",
-        "price": "<integer>",
+        "sellerID": seller_id,
+        "name": "Товар",
+        "price": random.randint(10, 10_000),
         "statistics": {
-            "likes": "<integer>",
-            "viewCount": "<integer>",
-            "contacts": "<integer>"
+            "likes": random.randint(0, 100),
+            "viewCount": random.randint(0, 1000),
+            "contacts": random.randint(0, 10)
         }
-        }
+    }
+
+
+class TestAvitoAPI:
+    def test_create_item(self):
+        pass
+
+    def test_get_item_by_id(self):
+        pass
+        
+    def test_get_items_by_seller_id(self):
+        pass
+    
+    
+    def test_get_statistic(self):
+        pass
+    
+    
+    def test_item_not_found(self):
+        pass
+    
+    
